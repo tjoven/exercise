cb4f095afd7316049c696b699072b76bff868ac2
zhaowen.tian <zhaowen.tian@ucarinc.com>
2020-06-30 11:11:04 +0800

    glide

---
+++ imageloader/src/main/java/com/example/imageloader/EmptyActivity.java
@@ -0,0 +1,13 @@
+package com.example.imageloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ imageloader/src/main/java/com/example/imageloader/glide/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.imageloader.glide;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.imageloader.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button,buttonGif,bt_quiet;
+    String url= "https://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+    String urlGIF= "https://p1.pstatp.com/large/166200019850062839d3";
+
+    RequestOptions options = new RequestOptions()
+            .placeholder(R.drawable.ic_launcher_background)
+            .error(R.drawable.ic_launcher_foreground)
+            //缓存策略
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image);
+        button = findViewById(R.id.button);
+        buttonGif = findViewById(R.id.buttonGif);
+        bt_quiet = findViewById(R.id.bt_quiet);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageLoader(v,url);
+            }
+        });
+
+        buttonGif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageLoader(v,urlGIF);
+            }
+        });
+
+        bt_quiet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageLoaderQuiet(v,urlGIF);
+            }
+        });
+
+
+    }
+
+    private void imageLoaderQuiet(View v,String pathurl) {
+//        Glide.with(this).load(url).into(imageView);
+
+        Glide.with(this)
+                .asBitmap()
+                .load(pathurl)
+                .apply(options)
+                .into(imageView);
+
+    }
+
+    private void imageLoader(View v,String pathurl) {
+
+        Glide.with(this)
+                .load(pathurl)
+              .apply(options)
+              .into(imageView);
+
+    }
+}
